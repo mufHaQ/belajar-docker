d97ec78b839c68866eeb16f2ecfb013aebebc373
mufHaQ <masbrowmess33@gmail.com>
2021-09-18 16:21:02 +0700

    add for docker env

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ app.listen(8090, () => console.log(`Listening at http://localhost:8090`));
 app.get("/", (req, res) => {
   res.json({
     date: new Date().toLocaleString("id-id").split("-").join("/").split(".").join(":"),
-    msg: "Hello World",
+    msg: process.env.MSG,
     version: process.version,
   });
 });
